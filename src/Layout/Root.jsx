3d13bbce774775707b495e7843686a6eb9389105
Raihan <raihanuddinahmed10@gmail.com>
2025-08-14 01:36:57 +0600

    Basic setup of React router dom with Tailwind CSS

---
+++ src/Layout/Root.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Root = () => {
+  return (
+    <div>
+      <h2 className="text-center text-5xl flex justify-center items-center min-h-screen">
+        React-router <span className="px-3 font-semibold italic"> v6</span> with
+        Tailwind CSS <span className="px-3 font-semibold italic"> v3</span>
+      </h2>
+    </div>
+  );
+};
+
+export default Root;
