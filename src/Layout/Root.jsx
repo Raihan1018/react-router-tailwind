edaeebe331f43fa28ddb7b089fa7d716fe2727ad
Raihan <raihanuddinahmed10@gmail.com>
2025-08-14 23:17:26 +0600

    first commit

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import NavBar from "../Components/NavBar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation, useNavigation } from "react-router-dom";
 
 const Root = () => {
+  const navigation = useNavigation();
+  const location = useLocation();
+  console.log(location);
   return (
     <>
       <NavBar />
-      <main className="flex justify-center my-10">
-        <Outlet />
-      </main>
+      <h1 className="text-center text-slate-400">{location.pathname}</h1>
+      {navigation.state === "loading" ? (
+        <p className="flex justify-center items-center text-7xl text-red-500 min-h-screen">
+          Loading...
+        </p>
+      ) : (
+        <main className="flex justify-center my-10">
+          <Outlet />
+        </main>
+      )}
     </>
   );
 };
